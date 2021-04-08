2155a632f0f30258d2f0e9374e789480701d7b5e
makadown <maseflores79@gmail.com>
2021-04-07 21:09:24 -0500

    Conociendo customHooks

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-    const misCategorias = ['Hajime no Ippo'];
+    const misCategorias = ['AVGN'];
     const [categories, setCategories] = useState(misCategorias);
 
     /*const handleAdd = () => {
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,22 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect( () => {
-       // console.log('Buscando por', category);
-        getGifs(category).then( imgs => {
-            setImages(imgs)
-        });
-    }, [category]); // si la category cambia, se vuelve a cargar el efecto
-
+    const { data: images, loading } = useFetchGifs(category);
     
     return (
         <>
         <h3> {category} </h3>
+        { loading && <p>Loading</p>}
         <div className="card-grid">
             
             {
