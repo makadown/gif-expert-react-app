6f3f00d9da39d49a47a74597c19aaaa796abdf61
makadown <maseflores79@gmail.com>
2021-04-10 19:04:50 -0500

    usando waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -4,15 +4,28 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas a Hook de useFetchGifs', () => {
     
-    test('Debe de retornar el estado inicial', () => {
+    test('Debe de retornar el estado inicial', async () => {
         const category = 'Berserk';
-        const { result } = renderHook( () => useFetchGifs(category));
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs(category));
         const {data, loading} = result.current;
         
+        await waitForNextUpdate();
+
         expect(data).toEqual([]);
         expect(loading).toBeTruthy();
         
     });
+
+    test('Debe retornar arreglo de imagenes y loading en false', async() => {
+        const category = 'One Punch';
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs(category));
+        await waitForNextUpdate();
+        const {data, loading} = result.current;
+        
+        expect(data.length).toBeGreaterThan(0);
+        expect(loading).toBeFalsy();
+    })
+    
     
 });
 
